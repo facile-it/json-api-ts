b3fd27447200e01f25deb19138470d6053012a27
Alberto Villa <alberto.villa@facile.it>
2019-11-22 19:35:17 +0100

    Remove useless codec decorator

--- src/io/DataWrappedC.ts
+++
@@ -1,5 +0,0 @@
-import * as t from 'io-ts';
-
-export const DataWrappedC = <C extends t.Mixed>(codec: C) => t.type({
-  data: codec
-});
--- src/io/RelationshipsC.ts
+++ src/io/RelationshipsC.ts
@@ -1,13 +1,12 @@
 import * as t from 'io-ts';
-import {DataWrappedC} from './DataWrappedC';
 import {ResourceIdentifierC} from './ResourceIdentifierC';
 
 export const RelationshipsC = t.record(
   t.string,
-  DataWrappedC(
-    t.union([
+  t.type({
+    data: t.union([
       ResourceIdentifierC,
       t.array(ResourceIdentifierC)
     ])
-  )
+  })
 );
