1cbf880aed8e7559afbb774b19ac5d83faabbdf5
Alberto Villa <alberto.villa@facile.it>
2019-11-25 15:55:01 +0100

    Cleanup

--- src/io/ArrayC.ts
+++ src/io/ArrayC.ts
@@ -1,10 +1,10 @@
 import {identity} from 'fp-ts/lib/function';
 import * as t from 'io-ts';
 
-const is = <T>() => (u: unknown): u is Array<T> => u instanceof Array;
+const is = <A>() => (u: unknown): u is Array<A> => u instanceof Array;
 
 export const ArrayC = <A = never>() => new t.Type<Array<A>>(
-  'UnknownArray',
+  'Array',
   is<A>(),
   (u, c) => is<A>()(u)
     ? t.success(u)
