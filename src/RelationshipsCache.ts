1f5bc53a900a671cd6a7fcb5667f35fbe934ee73
Alberto Villa <alberto.villa@facile.it>
2019-11-27 13:01:24 +0100

    Add counter of all resources

--- src/RelationshipsCache.ts
+++ src/RelationshipsCache.ts
@@ -12,46 +12,51 @@ export interface RelationshipsCache extends t.TypeOf<typeof RelationshipsCacheC>
 const fromRelationships = (x: RelationshipsCache | RelationshipsRecord): RelationshipsCache =>
   RelationshipsCacheC.is(x)
     ? x
-    : [{}, x];
+    : [0, {}, x];
 
 const lenses = {
-  global: new Lens<RelationshipsCache, RelationshipsRecord>(
+  counter: new Lens<RelationshipsCache, number>(
     s => s[0],
-    a => s => ([a, s[1]])
+    a => s => ([a, s[1], s[2]])
+  ),
+  global: new Lens<RelationshipsCache, RelationshipsRecord>(
+    s => s[1],
+    a => s => ([s[0], a, s[2]])
   ),
   local: new Lens<RelationshipsCache, RelationshipsRecord>(
-    s => s[1],
-    a => s => ([s[0], a])
+    s => s[2],
+    a => s => ([s[0], s[1], a])
   )
 };
 
 const monoid: Monoid<RelationshipsCache | RelationshipsRecord> = {
-  empty: [{}, {}],
+  empty: [0, {}, {}],
   concat: (x, y): RelationshipsCache => {
     const [xs, ys] = [x, y].map(fromRelationships) as [RelationshipsCache, RelationshipsCache];
+    const locals = Object.values(lenses.local.get(ys))
+      .reduce<Array<ResourceIdentifier>>(
+        (
+          array: Array<ResourceIdentifier>,
+          item: ResourceIdentifier | Array<ResourceIdentifier>
+        ): Array<ResourceIdentifier> => ([
+          ...array,
+          ...(ArrayC<ResourceIdentifier>().is(item) ? item : [item])
+        ]),
+        []
+      );
 
     return [
+      lenses.counter.get(xs) + (lenses.counter.get(ys) || locals.length),
       {
         ...lenses.global.get(xs),
         ...lenses.global.get(ys),
-        ...Object.values(lenses.local.get(ys))
-          .reduce<Array<ResourceIdentifier>>(
-            (
-              array: Array<ResourceIdentifier>,
-              item: ResourceIdentifier | Array<ResourceIdentifier>
-            ): Array<ResourceIdentifier> => ([
-              ...array,
-              ...(ArrayC<ResourceIdentifier>().is(item) ? item : [item])
-            ]),
-            []
-          )
-          .reduce(
-            (record: RelationshipsRecord, identifier: ResourceIdentifier): RelationshipsRecord => ({
-              ...record,
-              [ResourceIdentifier.iso.string.get(identifier)]: identifier
-            }),
-            {}
-          )
+        ...locals.reduce(
+          (record: RelationshipsRecord, identifier: ResourceIdentifier): RelationshipsRecord => ({
+            ...record,
+            [ResourceIdentifier.iso.string.get(identifier)]: identifier
+          }),
+          {}
+        )
       },
       {
         ...lenses.local.get(xs),
--- src/io/RelationshipsCacheC.ts
+++ src/io/RelationshipsCacheC.ts
@@ -2,6 +2,7 @@ import * as t from 'io-ts';
 import {RelationshipsRecordC} from './RelationshipsRecordC';
 
 export const RelationshipsCacheC = t.tuple([
+  t.number,
   RelationshipsRecordC,
   RelationshipsRecordC
 ]);
