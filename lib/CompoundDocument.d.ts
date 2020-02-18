fb2833c950e9eb941e9550a88643422b30800a3d
Alberto Villa <alberto.villa@facile.it>
2020-02-18 12:31:09 +0100

    Update lib/

--- lib/CompoundDocument.d.ts
+++ lib/CompoundDocument.d.ts
@@ -1,7 +1,11 @@
 import { Writer } from 'fp-ts/lib/Writer';
 import { RelationshipsCache } from './RelationshipsCache';
 import { RelationshipsRecord } from './RelationshipsRecord';
+import { UnknownRecord } from './UnknownRecord';
 export declare type CompoundDocument<A> = Writer<RelationshipsCache | RelationshipsRecord, A>;
 export declare const CompoundDocument: {
+    fromArray: (u: unknown[]) => CompoundDocument<unknown[]>;
     fromJson: (u: unknown, primaryData?: boolean) => CompoundDocument<unknown>;
+    fromRecord: (u: UnknownRecord) => CompoundDocument<UnknownRecord>;
+    fromUnknown: (u: unknown) => CompoundDocument<unknown>;
 };
