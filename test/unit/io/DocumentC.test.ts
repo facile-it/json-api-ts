be3e533695569a6b4277af313fcbbc5cb5a4e8f4
Alberto Villa <alberto.villa@facile.it>
2020-02-18 02:46:33 +0100

    Add some more tests to DocumentC

--- test/unit/io/DocumentC.test.ts
+++ test/unit/io/DocumentC.test.ts
@@ -5,47 +5,72 @@ describe('DocumentC', () => {
   describe('is', () => {
     it('should return false with invalid JSON:API documents', () => {
       expect(DocumentC.is({})).to.be.false;
-
+      expect(DocumentC.is([])).to.be.false;
       expect(DocumentC.is({data: false})).to.be.false;
       expect(DocumentC.is({data: 0})).to.be.false;
       expect(DocumentC.is({data: ''})).to.be.false;
-
       expect(DocumentC.is({data: {type: false}})).to.be.false;
       expect(DocumentC.is({data: {type: ''}})).to.be.false;
       expect(DocumentC.is({data: {id: false}})).to.be.false;
       expect(DocumentC.is({data: {id: ''}})).to.be.false;
-      expect(DocumentC.is({data: {attributes: false}})).to.be.false;
-      expect(DocumentC.is({data: {relationships: false}})).to.be.false;
-      expect(DocumentC.is({data: {relationships: {a: false}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: []}})).to.be.false;
       expect(DocumentC.is({data: {relationships: {a: {}}}})).to.be.false;
-      expect(DocumentC.is({data: {relationships: {a: {type: 'a'}}}})).to.be.false;
-      expect(DocumentC.is({data: {relationships: {a: {id: '1'}}}})).to.be.false;
-      expect(DocumentC.is({data: {relationships: {a: [{}]}}})).to.be.false;
-      expect(DocumentC.is({data: {relationships: {a: [{type: 'a'}]}}})).to.be.false;
-      expect(DocumentC.is({data: {relationships: {a: [{id: '1'}]}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: {}}}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: {type: 'a'}}}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: {id: '1'}}}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: {type: '', id: ''}}}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: [{}]}}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: [{type: 'a'}]}}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: [{id: '1'}]}}}})).to.be.false;
+      expect(DocumentC.is({data: {relationships: {a: {data: [{type: '', id: ''}]}}}})).to.be.false;
+      expect(DocumentC.is({data: null, included: {}})).to.be.false;
+      expect(DocumentC.is({data: null, included: [{}]})).to.be.false;
+      expect(DocumentC.is({data: null, included: [{type: 'a'}]})).to.be.false;
+      expect(DocumentC.is({data: null, included: [{id: '1'}]})).to.be.false;
+      expect(DocumentC.is({data: null, included: [{type: '', id: ''}]})).to.be.false;
     });
 
     it('should return false with arrays of non-resources (gcanti/io-ts#407)', () => {
       expect(DocumentC.is({data: ['']})).to.be.false;
+      expect(DocumentC.is({data: {attributes: []}})).to.be.false;
       expect(DocumentC.is({data: [{type: false}]})).to.be.false;
       expect(DocumentC.is({data: [{type: ''}]})).to.be.false;
       expect(DocumentC.is({data: [{id: false}]})).to.be.false;
       expect(DocumentC.is({data: [{id: ''}]})).to.be.false;
-      expect(DocumentC.is({data: [{attributes: false}]})).to.be.false;
-      expect(DocumentC.is({data: [{relationships: false}]})).to.be.false;
-      expect(DocumentC.is({data: [{relationships: {a: false}}]})).to.be.false;
+      expect(DocumentC.is({data: [{attributes: []}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: []}]})).to.be.false;
       expect(DocumentC.is({data: [{relationships: {a: {}}}]})).to.be.false;
-      expect(DocumentC.is({data: [{relationships: {a: {type: 'a'}}}]})).to.be.false;
-      expect(DocumentC.is({data: [{relationships: {a: {id: '1'}}}]})).to.be.false;
-      expect(DocumentC.is({data: [{relationships: {a: [{}]}}]})).to.be.false;
-      expect(DocumentC.is({data: [{relationships: {a: [{type: 'a'}]}}]})).to.be.false;
-      expect(DocumentC.is({data: [{relationships: {a: [{id: '1'}]}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: {}}}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: {type: 'a'}}}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: {id: '1'}}}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: {type: '', id: ''}}}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: [{}]}}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: [{type: 'a'}]}}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: [{id: '1'}]}}}]})).to.be.false;
+      expect(DocumentC.is({data: [{relationships: {a: {data: [{type: '', id: ''}]}}}]})).to.be.false;
     });
 
     it('should return true with valid JSON:API documents', () => {
       expect(DocumentC.is({data: null})).to.be.true;
       expect(DocumentC.is({data: {}})).to.be.true;
+      expect(DocumentC.is({data: []})).to.be.true;
       expect(DocumentC.is({data: [{}]})).to.be.true;
+      expect(DocumentC.is({data: {type: 'a'}})).to.be.true;
+      expect(DocumentC.is({data: {id: '1'}})).to.be.true;
+      expect(DocumentC.is({data: {attributes: {}}})).to.be.true;
+      expect(DocumentC.is({data: {relationships: {}}})).to.be.true;
+      expect(DocumentC.is({data: {relationships: {a: {data: {type: 'a', id: '1'}}}}})).to.be.true;
+      expect(DocumentC.is({data: {relationships: {a: {data: []}}}})).to.be.true;
+      expect(DocumentC.is({data: {relationships: {a: {data: [{type: 'a', id: '1'}]}}}})).to.be.true;
+      expect(DocumentC.is({data: [{type: 'a'}]})).to.be.true;
+      expect(DocumentC.is({data: [{id: '1'}]})).to.be.true;
+      expect(DocumentC.is({data: [{attributes: {}}]})).to.be.true;
+      expect(DocumentC.is({data: [{relationships: {}}]})).to.be.true;
+      expect(DocumentC.is({data: [{relationships: {a: {data: {type: 'a', id: '1'}}}}]})).to.be.true;
+      expect(DocumentC.is({data: [{relationships: {a: {data: []}}}]})).to.be.true;
+      expect(DocumentC.is({data: [{relationships: {a: {data: [{type: 'a', id: '1'}]}}}]})).to.be.true;
+      expect(DocumentC.is({data: null, included: []})).to.be.true;
+      expect(DocumentC.is({data: null, included: [{type: 'a', id: '1'}]})).to.be.true;
     });
   });
 });
